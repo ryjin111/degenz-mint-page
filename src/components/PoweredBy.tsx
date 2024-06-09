66f55a0635d166c72df2ca6739156edbdedf94ea
Clint <81433235+ryjin111@users.noreply.github.com>
2024-06-09 18:09:01 +0800

    edited with degenz contract

---
+++ src/components/PoweredBy.tsx
@@ -0,0 +1,12 @@
+export const PoweredBy: React.FC = () => {
+  return (
+    <div
+      onClick={() => {
+        window.open("https://thirdweb.com?utm_source=embed", "_blank");
+      }}
+      className="spacing-1 absolute bottom-4 right-4 flex cursor-pointer items-center"
+    >
+    
+    </div>
+  );
+};
